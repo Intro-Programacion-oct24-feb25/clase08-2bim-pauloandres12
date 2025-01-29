fa0bec12755eb88d9e4abf09eff55993ca8c0b8e
Paulo Andres <pbustamante@utpl.edu.ec>
2025-01-29 12:50:57 -0500

    cambios completos

--- Ejercicios07/src/ejemplo002/Principal1.java
+++ Ejercicios07/src/ejemplo002/Principal1.java
@@ -10,31 +10,40 @@ package ejemplo002;
 public class Principal1 {
 
     public static void main(String[] args) {
-        
-        String [] nombres = { "Jason", "Jonathan", "Kristen", "Robin", "Michelle", "Emily", "Noah", "Daniel"};
-        String [] apellidos = { "Lynch", "George", "Lang", "Cochran", "Young", "Fletcher", "Adkins", "Harris"};
-        int [][] notas = { {10, 80, 80, 95}, {40, 80, 80, 45}, {80, 10, 20, 55}, {70, 30, 20, 65}, 
-            {60, 50, 70, 75}, {50, 70, 30, 85},{40, 80, 40, 45}, {30, 90, 50, 95}};
-        
+
+        String[] nombres = {"Jason", "Jonathan", "Kristen", "Robin", "Michelle", "Emily", "Noah", "Daniel"};
+        String[] apellidos = {"Lynch", "George", "Lang", "Cochran", "Young", "Fletcher", "Adkins", "Harris"};
+        int[][] notas = {{10, 80, 80, 95}, {40, 80, 80, 45}, {80, 10, 20, 55}, {70, 30, 20, 65},
+        {60, 50, 70, 75}, {50, 70, 30, 85}, {40, 80, 40, 45}, {30, 90, 50, 95}};
+
         double promedio_paralelo = obtenerPromedioParalelo(notas);
         String nombre;
         String apellido;
         String tipoNotas;
+        String username;
+        int notaBaja;
+        int notaAlta;
         double promedioEstudiante;
         int numeroNotasArribaPromedio;
-        int [] filaNotas;
+        int[] filaNotas;
         String mensajeFinal = "";
         for (int i = 0; i < nombres.length; i++) {
             nombre = nombres[i];
             apellido = apellidos[i];
             filaNotas = notas[i];
             promedioEstudiante = funcion01(filaNotas);
-            numeroNotasArribaPromedio = funcion02(filaNotas, 
+            numeroNotasArribaPromedio = funcion02(filaNotas,
                     promedio_paralelo);
             tipoNotas = funcion03(filaNotas);
-            mensajeFinal = String.format("%s%s\n", mensajeFinal, 
-                    presentarReporte(nombre, apellido, tipoNotas, 
-                    promedioEstudiante, numeroNotasArribaPromedio));
+
+            username = funcion04(nombre, apellido);
+            notaBaja = funcion05(filaNotas);
+            notaAlta = funcion06(filaNotas);
+
+            mensajeFinal = String.format("%s%s\n", mensajeFinal,
+                    presentarReporte(nombre, apellido, username, tipoNotas,
+                            promedioEstudiante, numeroNotasArribaPromedio, notaBaja,
+                            notaAlta));
             // crea una cadena acumuladora llamada mensaje final, en la que
             // envia 2 parametros, mensaje final para acumular, y el valor que 
             // devuelva la funcion presentarReporte, al cual se le envian
@@ -45,22 +54,24 @@ public class Principal1 {
         // CrearArchivoTexto, en la cual, usando el formatter se crea el archivo
 
     }
-    
-    public static String presentarReporte(String nom, String ap, String notas, 
-            double prom, int numeroNotas){
+
+    public static String presentarReporte(String nom, String ap, String user,
+            String notas, double prom, int numeroNotas, int notaB, int notaA) {
         String reporte = String.format("Nombres: %s\n"
                 + "Apellidos: %s\n"
+                + "Username: %s\n"
                 + "Con notas: \n"
                 + "%s\n"
                 + "Promedio - %2f\n"
-                + "Número de notas arriba del promedio: %d\n\n",
-                nom, ap, notas, prom, numeroNotas);
-        
+                + "Número de notas arriba del promedio: %d\n"
+                + "Nota mas baja: %d\n"
+                + "Nota mas alta: %d\n",
+                nom, ap, user, notas, prom, numeroNotas, notaB, notaA);
+
         return reporte;
     }
 
-    
-    public static double obtenerPromedioParalelo(int [][] n){
+    public static double obtenerPromedioParalelo(int[][] n) {
         int suma = 0;
         double promedio;
         int contador = 0;
@@ -70,12 +81,12 @@ public class Principal1 {
                 contador = contador + 1;
             }
         }
-        
-        promedio = (double)suma/ contador;
+
+        promedio = (double) suma / contador;
         return promedio;
     }
-    
-    public static double funcion01(int [] notas){
+
+    public static double funcion01(int[] notas) {
         int suma = 0;
         double promedio;
         for (int i = 0; i < notas.length; i++) {
@@ -84,9 +95,9 @@ public class Principal1 {
         promedio = (double) suma / notas.length;
         return promedio;
     }
-    
-    public static int funcion02(int [] notas, double promedio){
-        
+
+    public static int funcion02(int[] notas, double promedio) {
+
         int contador = 0;
         int nota;
         for (int i = 0; i < notas.length; i++) {
@@ -94,36 +105,69 @@ public class Principal1 {
             if (nota > promedio) {
                 contador = contador + 1;
             }
-            
+
         }
-        
+
         return contador;
     }
-    
-    public static String funcion03(int [] notas){
+
+    public static String funcion03(int[] notas) {
         String cadena = "";
-        
+
         int nota;
         for (int i = 0; i < notas.length; i++) {
             nota = notas[i];
-            if (nota >=0 && nota<=20) {
+            if (nota >= 0 && nota <= 20) {
                 cadena = String.format("%s%d-%s\n", cadena, nota, "M");
-            }else{
-                if (nota >20 && nota<=50) {
+            } else {
+                if (nota > 20 && nota <= 50) {
                     cadena = String.format("%s%d-%s\n", cadena, nota, "MB");
-                }else{
+                } else {
                     if (nota > 50) {
                         cadena = String.format("%s%d-%s\n", cadena, nota, "S");
                     }
                 }
             }
-            
+
         }
-        
+
         return cadena;
     }
-    
-    
-    
-    
+
+    public static String funcion04(String nom, String ap) {
+        String cadena = "";
+
+        String nombre = nom.toLowerCase();
+        nombre = nombre.substring(0, 1);
+        String apellido = ap.toLowerCase();
+
+        cadena = String.format("%s%s.%s@utpl.edu.ec",
+                cadena,
+                nombre,
+                apellido);
+
+        return cadena;
+    }
+
+    public static int funcion05(int[] notas) {
+        int notaBaja = notas[0];
+        for (int i = 0; i < notas.length; i++) {
+            if (notas[i] < notaBaja) {
+                notaBaja = notas[i];
+            }
+        }
+        return notaBaja;
+    }
+
+    public static int funcion06(int[] notas) {
+        int notaAlta = notas[0];
+        for (int i = 0; i < notas.length; i++) {
+            if (notas[i] > notaAlta) {
+                notaAlta = notas[i];
+            }
+        }
+        return notaAlta;
+
+    }
+
 }
